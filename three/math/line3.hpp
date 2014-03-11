cc78fba5ee1a6a8852aea775775668ed4977dfd0
Elwin Arens <elwinjoeriarens@gmail.com>
2014-03-11 23:07:28 +0100

    math/Line3 code review: inline, ctor params

--- three/math/line3.hpp
+++ three/math/line3.hpp
@@ -6,60 +6,82 @@
 namespace three {
 
 class Line3 {
+
 public:
 
-	Line3() {
-		start = Vector3();
-		end = Vector3();
-	}
+	Line3() : start( Vector3() ), end( Vector3() ) {}
 
-	Line3( const Vector3& startIn, const Vector3& endIn) : start(startIn), end(endIn) {}
+	Line3( Vector3 startIn, Vector3 endIn) : start( startIn ), end( endIn ) {}
 
-	Line3& set( const Vector3& startIn, const Vector3& endIn ) {
+  Vector3 start, end;
+
+	inline Line3& set( const Vector3& startIn, const Vector3& endIn ) {
+
 		start.copy( startIn );
 		end.copy( endIn );
+
 		return *this;
+
 	}
 
-	Line3& copy( const Line3& line ) {
+	inline Line3& copy( const Line3& line ) {
+
 		start.copy( line.start );
 		end.copy( line.end );
+
 		return *this;
+
 	}
 
-	Vector3 center() const {
+	inline Vector3 center() const {
+
 		return Vector3().addVectors( start, end ).multiplyScalar( 0.5f );
+
 	}
 
-	Vector3& center( Vector3& target ) {
+	inline Vector3& center( Vector3& target ) {
+
 		return target.addVectors( start, end ).multiplyScalar( 0.5f );
+
 	}
 
-	Vector3 delta() const {
+	inline Vector3 delta() const {
+
 		return Vector3().subVectors( end, start );
+
 	}
 
-	Vector3& delta( Vector3& target ) {
+	inline Vector3& delta( Vector3& target ) {
+
 		return target.subVectors( end, start );
+
 	}
 
-	float distanceSq() {
+	inline float distanceSq() {
+
 		return start.distanceToSquared( end );
+
 	}
 
-	float distance() {
+	inline float distance() {
+
 		return start.distanceTo( end );
+
 	}
 
-	Vector3 at( float t ) {
+	inline Vector3 at( float t ) {
+
 		return delta().multiplyScalar( t ).add( start );
+
 	}
 
-	Vector3& at( float t, Vector3& target ) {
+	inline Vector3& at( float t, Vector3& target ) {
+
 		return delta( target ).multiplyScalar( t ).add( start );
+
 	}
 
-	float closestPointToPointParameter( const Vector3& point, bool clampToLine ) const {
+	inline float closestPointToPointParameter( const Vector3& point, bool clampToLine ) const {
 
 		auto startP = Vector3();
 		auto startEnd = Vector3();
@@ -73,36 +95,50 @@ public:
 		auto t = startEnd_startP / startEnd2;
 
 		if ( clampToLine ) {
+
 			t = Math::clamp( t, 0.f, 1.f );
+
 		}
+
 		return t;
 	}
 
-	Vector3 closestPointToPoint( const Vector3& point, bool clampToLine ) {
+	inline Vector3 closestPointToPoint( const Vector3& point, bool clampToLine ) {
+
 		auto t = closestPointToPointParameter( point, clampToLine );
+
 		return delta().multiplyScalar( t ).add( start );
+
 	}
 
-	Vector3& closestPointToPoint( const Vector3& point, bool clampToLine, Vector3& target ) {
+	inline Vector3& closestPointToPoint( const Vector3& point, bool clampToLine, Vector3& target ) {
+
 		auto t = closestPointToPointParameter( point, clampToLine );
+
 		return delta( target ).multiplyScalar( t ).add( start );
+
 	}
 
-	Line3& applyMatrix4( const Matrix4& matrix ) {
+	inline Line3& applyMatrix4( const Matrix4& matrix ) {
+
 		start.applyMatrix4( matrix );
 		end.applyMatrix4( matrix );
+
 		return *this;
+
 	}
 
-	bool equals( const Line3& line ) const {
+	inline bool equals( const Line3& line ) const {
+
 		return line.start.equals( start ) && line.end.equals( end );
+
 	}
 
-	Line3 clone() {
+	inline Line3 clone() {
+
 		return *this;
-	}
 
-  Vector3 start, end;
+	}
   
 };
     
