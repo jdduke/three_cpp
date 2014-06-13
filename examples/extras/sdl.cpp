df2a9e91c83c99ab797661f0f4f26347c6622355
Elwin Arens <elwinjoeriarens@gmail.com>
2014-06-13 08:05:43 +0200

    Set gl depth size to 24 due to near plane artifacts on mac osx

--- examples/extras/sdl.cpp
+++ examples/extras/sdl.cpp
@@ -48,7 +48,8 @@ GLWindow::GLWindow( const RendererParameters& parameters )
   SDL_GL_SetAttribute( SDL_GL_CONTEXT_MAJOR_VERSION, 2 );
   SDL_GL_SetAttribute( SDL_GL_CONTEXT_MINOR_VERSION, 1 );
   SDL_GL_SetAttribute( SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_COMPATIBILITY );
-
+  SDL_GL_SetAttribute( SDL_GL_DEPTH_SIZE, 24 );
+  
   if ( parameters.vsync )
     SDL_GL_SetSwapInterval( 1 );
 
